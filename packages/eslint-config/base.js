21a0694a1d589d89127a237230a2b1ef4e5c40e6
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2026-04-26 23:58:26 +0100

    chore: tighten shared package configs

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -8,5 +8,13 @@ module.exports = {
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module"
-  }
+  },
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint"],
+      extends: ["plugin:@typescript-eslint/recommended"]
+    }
+  ]
 };
